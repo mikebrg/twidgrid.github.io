6981f90dfd344ee369f2e89dcea99cc21606e59f
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2024-09-04 10:35:52 +0200

    updates

---
+++ music/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,6 @@
+
+var tipuesearch = {"pages": [
+
+     {"title": "	A Bar Song (Tipsy)	", "text": "	A Bar Song (Tipsy) is song by Shaboozey released in 2024	", "img": "	assets/images/a-bar-song-tipsy-shaboozey-2024-340x340.jpg	", "tags": "	Shaboozey	", "note": "	2024	", "url": "	https://www.youtube.com/results?search_query=	A Bar Song (Tipsy) - Shaboozey - 2024	"},
+               
+     ]};
