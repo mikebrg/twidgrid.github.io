da8ef9e9fdc336d7c4db4095f0f2a4dfef2b7ea2
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2025-08-14 14:44:09 +0200

    Add files via upload

--- movies.js
+++ movies.js
@@ -1,8 +1,6 @@
 window.categorySites = window.categorySites || {};
 window.categorySites["Movies"] = [
 
-  { name: "The Shawshank Redemption", imdb: "https://www.imdb.com/title/tt0111161/" },
-  { name: "Inception", imdb: "https://www.imdb.com/title/tt1375666/" },
-  { name: "Interstellar", imdb: "https://www.imdb.com/title/tt0816692/" }
+ { name: "	Wings (1927)	", url: "	https://www.imdb.com/title/tt0018578/	", image: "movies/img/	Wings (1927).webp	" },
 
 ];
