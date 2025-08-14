415a880d0082267b1890915ea3384d0cd716b819
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2025-08-14 20:31:22 +0200

    Update movies.js

--- movies.js
+++ movies.js
@@ -2,16 +2,16 @@ window.categorySites = window.categorySites || {};
 
 window.categorySites["Movies"] = [
 
-{ name: "	Avengers: Doomsday (2026)	", url: "https://www.imdb.com/title/	tt21357150	", image: "images/movies/	avengers-5-600x900.jpg	" },
-{ name: "	Fast X: Part 2 (2026)	", url: "https://www.imdb.com/title/	tt27550022	", image: "images/movies/	fast-x-part-2-600x900.jpg	" },
-{ name: "	Project Hail Mary (2026)	", url: "https://www.imdb.com/title/	tt12042730	", image: "images/movies/	Project Hail Mary (2026).webp	" },
-{ name: "	Shrek 5 (2026)	", url: "https://www.imdb.com/title/	tt6113186	", image: "images/movies/	shrek-5-600x900.jpg	" },
-{ name: "	Spider-Man: Brand New Day (2026)	", url: "https://www.imdb.com/title/	tt22084616	", image: "images/movies/	Spider-Man Brand New Day (2026).webp	" },
-{ name: "	Supergirl: Woman of Tomorrow (2026)	", url: "https://www.imdb.com/title/	tt8814476	", image: "images/movies/	supergirl-woman-of-tomorrow-1186x1186.jpg	" },
-{ name: "	The Batman - Part II (2026)	", url: "https://www.imdb.com/title/	tt19850008	", image: "images/movies/	the-batman-part-ii-375x563.jpg	" },
-{ name: "	The Hunger Games: Sunrise on the Reaping (2026)	", url: "https://www.imdb.com/title/	tt32558705	", image: "images/movies/	the-hunger-games-sunrise-on-the-reaping-376x564.jpg	" },
-{ name: "	The Lord of the Rings: The Hunt for Gollum (2026)	", url: "https://www.imdb.com/title/	tt32328070	", image: "images/movies/	the-lord-of-the-rings-the-hunt-for-gollum-600x900.jpg	" },
-{ name: "	Toy Story 5 (2026)	", url: "https://www.imdb.com/title/	tt29355505	", image: "images/movies/	toy-story-5-600x900.jpg	" },
+{ name: "	(2026)<p>Avengers: Doomsday	", url: "https://www.imdb.com/title/	tt21357150	", image: "images/movies/	avengers-5-600x900.jpg	" },
+{ name: "	(2026) Fast X: Part 2	", url: "https://www.imdb.com/title/	tt27550022	", image: "images/movies/	fast-x-part-2-600x900.jpg	" },
+{ name: "	(2026) Project Hail Mary	", url: "https://www.imdb.com/title/	tt12042730	", image: "images/movies/	Project Hail Mary (2026).webp	" },
+{ name: "	(2026) Shrek 5	", url: "https://www.imdb.com/title/	tt6113186	", image: "images/movies/	shrek-5-600x900.jpg	" },
+{ name: "	(2026) Spider-Man: Brand New Day	", url: "https://www.imdb.com/title/	tt22084616	", image: "images/movies/	Spider-Man Brand New Day (2026).webp	" },
+{ name: "	(2026) Supergirl: Woman of Tomorrow	", url: "https://www.imdb.com/title/	tt8814476	", image: "images/movies/	supergirl-woman-of-tomorrow-1186x1186.jpg	" },
+{ name: "	(2026) The Batman - Part II	", url: "https://www.imdb.com/title/	tt19850008	", image: "images/movies/	the-batman-part-ii-375x563.jpg	" },
+{ name: "	(2026) The Hunger Games: Sunrise on the Reaping	", url: "https://www.imdb.com/title/	tt32558705	", image: "images/movies/	the-hunger-games-sunrise-on-the-reaping-376x564.jpg	" },
+{ name: "	(2026) The Lord of the Rings: The Hunt for Gollum	", url: "https://www.imdb.com/title/	tt32328070	", image: "images/movies/	the-lord-of-the-rings-the-hunt-for-gollum-600x900.jpg	" },
+{ name: "	(2026) Toy Story 5	", url: "https://www.imdb.com/title/	tt29355505	", image: "images/movies/	toy-story-5-600x900.jpg	" },
 { name: "	Wings (1927)	", url: "	https://www.imdb.com/title/tt0018578/	", image: "images/movies/	Wings (1927).webp	" },
 
 ];
