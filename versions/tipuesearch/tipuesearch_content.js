0c0815f492d1731b22f601cc8d3cd089525fcae9
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2025-08-28 15:07:23 +0200

    Update tipuesearch_content.js

--- versions/tipuesearch/tipuesearch_content.js
+++ versions/tipuesearch/tipuesearch_content.js
@@ -1,6 +1,6 @@
 
 var tipuesearch = {"pages": [
 
-{"title": "	Adobe Photoshop	", "text": "	Adobe Photoshop is a raster graphics editor developed and published by Adobe for Windows and macOS. It was created in 1987 by Thomas and John Knoll. It is the most used tool for professional digital art, especially in raster graphics editing, and its name has become genericised as a verb (e.g. "to photoshop an image", "photoshopping", and "photoshop contest") although Adobe disapproves of such use.	", "img": "img/	Adobe Photoshop.jpg	", "tags": "	Raster Graphics Editor	", "note": "		", "url": "	index.html#:~:text=Adobe Photoshop	"},
+{"title": "	Adobe Photoshop	", "text": "	Adobe Photoshop is a raster graphics editor developed and published by Adobe for Windows and macOS. It was created in 1987 by Thomas and John Knoll. It is the most used tool for professional digital art, especially in raster graphics editing, and its name has become genericised as a verb (e.g. 'to photoshop an image', 'photoshopping', and 'photoshop contest') although Adobe disapproves of such use.	", "img": "img/	Adobe Photoshop.jpg	", "tags": "	Raster Graphics Editor	", "note": "		", "url": "	index.html#:~:text=Adobe Photoshop	"},
 	
 ]};
