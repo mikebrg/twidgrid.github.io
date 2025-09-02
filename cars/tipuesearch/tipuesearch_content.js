37eccf50aba5686bff71f59f4c72f41a4e106e75
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2025-09-02 13:55:28 +0200

    Update tipuesearch_content.js

--- cars/tipuesearch/tipuesearch_content.js
+++ cars/tipuesearch/tipuesearch_content.js
@@ -2,5 +2,7 @@
 var tipuesearch = {"pages": [
 
 {"title": "	Zenvo	", "text": "	Zenvo Automotive A/S is a Danish sports car manufacturer located in Præstø on the Danish Island of Zealand. Co-founded in 2007 by Jesper Jensen and Troels Vollertsen, the company ran under the name Nordic Sports Car A/S until 2009 when the company was rebranded to Zenvo Automotive to tie in the reveal of the ST1. In June 2018 the company was sold to Czech investors, based in Prague. The name 'Zenvo' is derived from a combination of the last three and first two letters in Vollertsen's last name.	", "img": "img/	Zenvo.jpg	", "tags": "		", "note": "		", "url": "	index.html#:~:text=Zenvo	"},	
+{"title": "	Aixam	", "text": "	Aixam-Mega is a French automobile manufacturer based in Aix-les-Bains, Savoie. It was founded in 1983 to make microcars following the acquisition of Arola. On 11 April 2013, US based Polaris Industries announced that it had acquired Aixam-Mega from previous owners Axa Private Equity.	", "img": "img/	Aixam.jpg	", "tags": "		", "note": "		", "url": "	index.html#:~:text=Aixam	"},
+  
 
 ]};
