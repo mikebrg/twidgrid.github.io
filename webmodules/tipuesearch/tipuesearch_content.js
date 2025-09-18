7cb9d667f2e2c461e8bdd9a310816062f989ab66
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2025-09-18 16:28:25 +0200

    Delete webmodules directory

--- webmodules/tipuesearch/tipuesearch_content.js
+++
@@ -1,6 +0,0 @@
-
-var tipuesearch = {"pages": [
-
-{"title": "	PDF Editor	", "text": "	Online PDF editor	", "img": "img/	PDF Editor.jpg	", "tags": "	Document Management / PDF Tools / Online Editors	", "note": "		", "url": "	index.html#:~:text=PDF Editor	"},
-	
-]};
