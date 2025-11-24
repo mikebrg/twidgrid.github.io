1a5ec68b0f5a2ad07c3aa6aa1225b3e90326b878
Michael Borg <156905433+mikebrg@users.noreply.github.com>
2025-11-24 15:20:18 +0100

    Update tipuesearch_content.js

--- cars/tipuesearch/tipuesearch_content.js
+++ cars/tipuesearch/tipuesearch_content.js
@@ -1,6 +1,8 @@
 
 var tipuesearch = {"pages": [
 
+  {"title": "	PGO	", "text": "	PGO Automobiles is a French car manufacturer, producing exclusive series' of sports cars. The brand operates in the same special market as Wiesmann, and Donkervoort. Their cars are considered an appropriate solution for connoisseurs seeking individuality and style.	", "img": "img/	PGO.jpg	", "tags": "		", "note": "		", "url": "	index.html#:~:text=PGO	"},
+
 {"title": "	Zenvo	", "text": "	Zenvo Automotive A/S is a Danish sports car manufacturer located in Præstø on the Danish Island of Zealand. Co-founded in 2007 by Jesper Jensen and Troels Vollertsen, the company ran under the name Nordic Sports Car A/S until 2009 when the company was rebranded to Zenvo Automotive to tie in the reveal of the ST1. In June 2018 the company was sold to Czech investors, based in Prague. The name 'Zenvo' is derived from a combination of the last three and first two letters in Vollertsen's last name.	", "img": "img/	Zenvo.jpg	", "tags": "		", "note": "		", "url": "	index.html#:~:text=Zenvo	"},	
 {"title": "	Aixam	", "text": "	Aixam-Mega is a French automobile manufacturer based in Aix-les-Bains, Savoie. It was founded in 1983 to make microcars following the acquisition of Arola. On 11 April 2013, US based Polaris Industries announced that it had acquired Aixam-Mega from previous owners Axa Private Equity.	", "img": "img/	Aixam.jpg	", "tags": "		", "note": "		", "url": "	index.html#:~:text=Aixam	"},
 {"title": "	Alpine	", "text": "	Société des Automobiles Alpine SAS, commonly known as Alpine, is a French manufacturer of sports cars and racing cars established in 1955. The Alpine car marque was created in 1954.	", "img": "img/	Alpine.jpg	", "tags": "		", "note": "		", "url": "	index.html#:~:text=Alpine	"},
